18faff088986ffb98ad7cca89899c97e7b995f6e
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-22 23:32:42 -0300

    Inicialização do vuejs
    - Definição do app.js
    - Criação do componente principal do Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import routes from './routes';
+
+import App from './components/App';
+
+Vue.prototype.$http = axios;
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+const app = new Vue({
+    el: '#app',
+    components: {App},
+    router
+})
