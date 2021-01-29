5fd35600d0dd4b68189ff92a6a8e664163ef8bb3
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-28 23:03:13 -0300

    Importação do v-mask

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,17 @@ require('./bootstrap');
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import axios from 'axios';
+import VueMask from 'v-mask';
+
 import routes from './routes';
 
 import App from './components/App';
 
 Vue.prototype.$http = axios;
+
+//módulos para uso nos componentes
 Vue.use(VueRouter);
+Vue.use(VueMask);
 
 const router = new VueRouter({
     mode: 'history',
