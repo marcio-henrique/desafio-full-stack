da58dc028ecceb7bc366bda884ac411dd0f8748c
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-29 15:05:28 -0300

    Rotas de front-end de providers

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,13 +1,14 @@
 import Index from './components/index'
 import CreateProvider from "./components/provider/CreateProvider";
 import ListProvider from "./components/provider/ListProvider";
+import ShowProvider from "./components/provider/ShowProvider";
 
 const routes = [
-        { path: '/', component: Index },
-        { path: '/provider', component: ListProvider },
-        { path: '/provider/add', component: CreateProvider },
+    { path: '/', name:'Index', component: Index },
+    { path: '/provider', name:'ListProvider', component: ListProvider },
+    { path: '/provider/view/:id', name:'ShowProvider', component: ShowProvider },
+    { path: '/provider/add', name:'CreateProvider', component: CreateProvider },
 
-
-    ];
+];
 
 export default routes;
