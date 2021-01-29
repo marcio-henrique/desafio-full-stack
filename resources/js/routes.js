caf31d76007d22458c51bd61d23de3d2bc0e6e7a
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-01-28 23:00:29 -0300

    Adição das rotas de front-end do provider

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,13 @@
 import Index from './components/index'
+import CreateProvider from "./components/provider/CreateProvider";
+import ListProvider from "./components/provider/ListProvider";
 
 const routes = [
         { path: '/', component: Index },
+        { path: '/provider', component: ListProvider },
+        { path: '/provider/add', component: CreateProvider },
+
+
     ];
 
 export default routes;
